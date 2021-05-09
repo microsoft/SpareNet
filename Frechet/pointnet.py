98504711345fcb52753c7413aceaf7639d21ffe6
XinYuan-believe <799586940@qq.com>
2021-05-09 21:19:00 +0800

    add FPD files

---
+++ Frechet/pointnet.py
@@ -0,0 +1,149 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+import torch.nn.functional as F
+
+"""PointNet: Deep Learning on Point Sets for 3D Classification and Segmentation"
+    github code  : (https://github.com/charlesq34/pointnet)
+    project page : (http://stanford.edu/~rqi/pointnet/)
+    paper        : (https://arxiv.org/abs/1612.00593)
+"""
+
+class STN3d(nn.Module):
+    def __init__(self):
+        super(STN3d, self).__init__()
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 9)
+        self.relu = nn.ReLU()
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
+
+        iden = Variable(torch.from_numpy(np.array([1,0,0,0,1,0,0,0,1]).astype(np.float32))).view(1,9).repeat(batchsize,1)
+        if x.is_cuda:
+            iden = iden.cuda()
+        x = x + iden
+        x = x.view(-1, 3, 3)
+        return x
+
+
+class PointNetfeat(nn.Module):
+    def __init__(self, global_feat = True):
+        super(PointNetfeat, self).__init__()
+        self.stn = STN3d()
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.global_feat = global_feat
+    def forward(self, x):
+        batchsize = x.size()[0]
+        n_pts = x.size()[2]
+        trans = self.stn(x)
+        x = x.transpose(2,1)
+        x = torch.bmm(x, trans)
+        x = x.transpose(2,1)
+        x = F.relu(self.bn1(self.conv1(x)))
+        pointfeat = x
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.bn3(self.conv3(x))
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+        if self.global_feat:
+            return x, trans
+        else:
+            x = x.view(-1, 1024, 1).repeat(1, 1, n_pts)
+            return torch.cat([x, pointfeat], 1), trans
+
+class PointNetCls(nn.Module):
+    def __init__(self, k = 2):
+        super(PointNetCls, self).__init__()
+        self.feat = PointNetfeat(global_feat=True)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, k)
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.relu = nn.ReLU()
+    def forward(self, x):
+        x1, trans = self.feat(x)
+        x2 = F.relu(self.bn1(self.fc1(x1)))
+        x3 = F.relu(self.bn2(self.fc2(x2)))
+        x4 = self.fc3(x3)
+        
+        actv = torch.cat((x1, x2, x3, x4), dim=1)
+        
+        return F.log_softmax(x4, dim=0), trans, actv
+
+class PointNetDenseCls(nn.Module):
+    def __init__(self, k = 2):
+        super(PointNetDenseCls, self).__init__()
+        self.k = k
+        self.feat = PointNetfeat(global_feat=False)
+        self.conv1 = torch.nn.Conv1d(1088, 512, 1)
+        self.conv2 = torch.nn.Conv1d(512, 256, 1)
+        self.conv3 = torch.nn.Conv1d(256, 128, 1)
+        self.conv4 = torch.nn.Conv1d(128, self.k, 1)
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.bn3 = nn.BatchNorm1d(128)
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        n_pts = x.size()[2]
+        x, trans = self.feat(x)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.conv4(x)
+        x = x.transpose(2,1).contiguous()
+        x = F.log_softmax(x.view(-1,self.k), dim=-1)
+        x = x.view(batchsize, n_pts, self.k)
+        return x, trans
+
+
+if __name__ == '__main__':
+    sim_data = Variable(torch.rand(32,3,2500))
+    trans = STN3d()
+    out = trans(sim_data)
+    print('stn', out.size())
+
+    pointfeat = PointNetfeat(global_feat=True)
+    out, _ = pointfeat(sim_data)
+    print('global feat', out.size())
+
+    pointfeat = PointNetfeat(global_feat=False)
+    out, _ = pointfeat(sim_data)
+    print('point feat', out.size())
+
+    cls = PointNetCls(k = 5)
+    out, _ = cls(sim_data)
+    print('class', out.size())
+
+    seg = PointNetDenseCls(k = 3)
+    out, _ = seg(sim_data)
+    print('seg', out.size())
---
+++ test_fpd.py
@@ -0,0 +1,140 @@
+import open3d as o3d
+import argparse
+import logging
+import torch
+import os
+import numpy as np
+from Frechet.FPD import calculate_fpd
+logger = logging.getLogger()
+
+def read_pcd(filename):
+    pcd = o3d.io.read_point_cloud(filename)
+    return torch.from_numpy(np.array(pcd.points)).float()
+
+
+def save_pcd(filename, points):
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    o3d.io.write_point_cloud(filename, pcd)
+
+def set_logger(filename):
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(levelname)s: - %(message)s")
+
+    fh = logging.FileHandler(filename)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--plot_freq', type=int, default=1)
+parser.add_argument('--save_pcd', action='store_true',default=False)
+parser.add_argument('--log_dir', default='/path/to/save/logs')
+parser.add_argument('--list_path', default='/path/to/test.list')
+parser.add_argument('--data_dir', default='/path/to/test/dataset')
+parser.add_argument('--fake_dir', default='/path/to/methods/pcds',
+                            help='/path/to/results/shapenet_fc/pcds/')
+parser.add_argument('--num_points', type=int, default=16384, help='number of points: 2048 or 8192')
+
+
+
+opt = parser.parse_args()
+os.makedirs(opt.log_dir, exist_ok=True)
+
+set_logger(os.path.join(opt.log_dir, "log.txt"))
+logger.info("save into dir: %s" % opt.log_dir)
+
+
+with open(opt.list_path) as file:
+    model_list = file.read().splitlines()
+
+
+# Testing loop
+n_samples = len(model_list)
+logger.info("n_samples %s"% n_samples)
+
+
+taxonomy2label = {
+    '02691156':0, 
+    '02933112':1, 
+    '02958343':2,
+    '03001627':3,
+    '03636649':4,
+    '04256520':5,
+    '04379243':6,
+    '04530566':7
+}
+
+
+label2taxonomy = {
+    0:'02691156', 
+    1:'02933112', 
+    2:'02958343',
+    3:'03001627',
+    4:'03636649',
+    5:'04256520',
+    6:'04379243',
+    7:'04530566'
+}
+
+fpd_values= []
+
+# test for each category 
+for batch_idx in range(0,8):
+    fake_pointclouds =  torch.Tensor([]).cuda()
+    real_pointclouds =  torch.Tensor([]).cuda()
+    # prepare the batch data
+    for idx, model_id in enumerate(model_list):
+        taxonomy_id, model_id_real = model_id.split('/')
+        if taxonomy2label[taxonomy_id] == batch_idx:
+            fake = torch.zeros((1, opt.num_points, 3), device='cuda')
+            gt = torch.zeros((1, opt.num_points, 3), device='cuda')
+            pcd = o3d.io.read_point_cloud(os.path.join(opt.fake_dir,  '%s.pcd' % model_id))
+            fake[0, :, :] = torch.from_numpy(np.array(pcd.points))
+            pcd = o3d.io.read_point_cloud(os.path.join(opt.data_dir, 'complete', '%s.pcd' % model_id))
+            gt[0, :, :] = torch.from_numpy(np.array(pcd.points))
+            fake_pointclouds = torch.cat((fake_pointclouds, fake), dim=0)
+            real_pointclouds = torch.cat((real_pointclouds, gt), dim=0)
+
+    # 150 data samples per batch 
+    fpd = calculate_fpd(fake_pointclouds, real_pointclouds, statistic_save_path=None, batch_size=30, dims=1808, device= fake_pointclouds.device)
+    fpd_values.append(fpd)
+    logger.info("[ %s  category] Frechet Pointcloud Distance <<< %f >>>"% (label2taxonomy[batch_idx], fpd))
+
+    del fake_pointclouds
+    del real_pointclouds
+
+logger.info("avg FPD for 8 categories: %f" % np.mean(fpd_values))
+
+# test the all test data 
+
+
+fake_pointclouds =  torch.Tensor([]).cuda()
+real_pointclouds =  torch.Tensor([]).cuda()
+# prepare the batch data
+for idx, model_id in enumerate(model_list):
+    taxonomy_id, model_id_real = model_id.split('/')
+    fake = torch.zeros((1, opt.num_points, 3), device='cuda')
+    gt = torch.zeros((1, opt.num_points, 3), device='cuda')
+    pcd = o3d.io.read_point_cloud(os.path.join(opt.fake_dir,  '%s.pcd' % model_id))
+    fake[0, :, :] = torch.from_numpy(np.array(pcd.points))
+    pcd = o3d.io.read_point_cloud(os.path.join(opt.data_dir, 'complete', '%s.pcd' % model_id))
+    gt[0, :, :] = torch.from_numpy(np.array(pcd.points))
+    fake_pointclouds = torch.cat((fake_pointclouds, fake), dim=0)
+    real_pointclouds = torch.cat((real_pointclouds, gt), dim=0)
+
+# 150 data samples per batch 
+fpd = calculate_fpd(fake_pointclouds, real_pointclouds, statistic_save_path=None, batch_size=30, dims=1808, device= fake_pointclouds.device)
+logger.info("[ all category] Frechet Pointcloud Distance <<< %f >>>"%  fpd)
+
+
+
+
